f246e332d2b51309a77a66dcc19b04c9040eb9a4
Tad <t.aleksandruk@gmail.com>
2019-07-12 19:19:32 +0100

    fixing weird bug with not being able to run

--- components/HelloWorld/HelloWorld.tsx
+++ components/HelloWorld/HelloWorld.tsx
@@ -1,76 +1,18 @@
 import React, { Component } from "react";
 
-import { getActions, getState } from "../../store";
-
-import Avatar from "../Avatar/Avatar";
-import Popover from "../Popover/Popover";
-
-import { Wrapper } from "./HelloWorld.style";
-
-import { IAvatar } from "../../model/avatars";
-
 interface IProps {
-  IpropsText?: number;
+  // props types go here
+  testProps: string;
 }
 
 interface IState {
-  popoverOpen: boolean;
-  active: IAvatar;
+  // state type goes here
+  testState: string;
 }
 
 class HelloWorld extends Component<IProps, IState> {
-  public static defaultProps = {
-    propsText: "Hello World"
-  };
-
-  public avatars: IAvatar[] = getState().avatars.items;
-
-  public state: any = {
-    active: this.avatars[0],
-    popoverOpen: false
-  };
-
-  public togglePopover = (popoverOpen: boolean): void => {
-    this.setState({ popoverOpen });
-  };
-
-  public selectAvatar = (index: number) => {
-    const active: IAvatar | null =
-      this.avatars.find(avatar => avatar.index === index) || null;
-
-    if (active) {
-      getActions().avatars.updateActive(active);
-      this.setState({
-        active,
-        popoverOpen: false
-      });
-    }
-  };
-
   public render() {
-    const restOfAvatars: IAvatar[] = this.avatars
-      .filter(avatar => avatar.index !== this.state.active.index)
-      .sort();
-
-    const styles = getState().avatars.active.styles || {};
-
-    return (
-      <Wrapper>
-        {/* active avatar */}
-        <Avatar active={true} onEnter={this.togglePopover} styles={styles} />
-        <Popover open={this.state.popoverOpen}>
-          {restOfAvatars.map(avatar => (
-            <Avatar
-              active={false}
-              key={avatar.index}
-              index={avatar.index}
-              styles={avatar.styles}
-              selectAvatar={this.selectAvatar}
-            />
-          ))}
-        </Popover>
-      </Wrapper>
-    );
+    return <div> Typescript, Styled components and Jest Boilerplate! </div>;
   }
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,16 @@
 import React from "react";
 
-import { StoreProvider } from "easy-peasy";
 import { ThemeProvider } from "styled-components";
 
 import HelloWorld from "../components/HelloWorld/HelloWorld";
 
-import store from "../store";
 import theme from "../styles/theme";
 
 function Home() {
   return (
-    <StoreProvider store={store}>
-      <ThemeProvider theme={theme}>
-        <HelloWorld />
-      </ThemeProvider>
-    </StoreProvider>
+    <ThemeProvider theme={theme}>
+      <HelloWorld testProps="test" />
+    </ThemeProvider>
   );
 }
 
